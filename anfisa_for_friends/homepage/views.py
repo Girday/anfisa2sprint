4247f9434894b745184b2710b9c7cd0b346e65c7
Maxim Grigorev <maxkraft06@gmail.com>
2026-01-01 18:00:57 +0300

    done the project

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template = "homepage/index.html"
+
+    ice_cream_list = (
+        IceCream.objects.filter(is_on_main=True, is_published=True)
+        .order_by("title")
+        .values("id", "title", "description", "wrapper__title")
+    )
+
+    context = {
+        "ice_cream_list": ice_cream_list,
+    }
+    return render(request, template, context)
